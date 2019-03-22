f48c1ec949bd68e236b919f9b4529aada08be42a
Michael Ray <michael.ray@arm.com>
2019-03-22 13:53:05 -0500

    Run BLE example as both a peripheral and a central node

--- BLE_LED/source/main.cpp
+++ BLE_LED/source/main.cpp
@@ -17,12 +17,73 @@
 #include <events/mbed_events.h>
 #include <mbed.h>
 #include "ble/BLE.h"
-#include "LEDService.h"
+#include "ble/DiscoveredCharacteristic.h"
+#include "ble/DiscoveredService.h"
+#include "ble/gap/Gap.h"
+#include "ble/gap/AdvertisingDataParser.h"
 #include "pretty_printer.h"
+#include "LEDService.h"
 
 const static char DEVICE_NAME[] = "LED";
 
 static EventQueue event_queue(/* event count */ 10 * EVENTS_EVENT_SIZE);
+
+static DiscoveredCharacteristic led_characteristic;
+static bool trigger_led_characteristic = false;
+
+void service_discovery(const DiscoveredService *service) {
+    if (service->getUUID().shortOrLong() == UUID::UUID_TYPE_SHORT) {
+        printf("S UUID-%x attrs[%u %u]\r\n", service->getUUID().getShortUUID(), service->getStartHandle(), service->getEndHandle());
+    } else {
+        printf("S UUID-");
+        const uint8_t *longUUIDBytes = service->getUUID().getBaseUUID();
+        for (unsigned i = 0; i < UUID::LENGTH_OF_LONG_UUID; i++) {
+            printf("%02x", longUUIDBytes[i]);
+        }
+        printf(" attrs[%u %u]\r\n", service->getStartHandle(), service->getEndHandle());
+    }
+}
+
+void update_led_characteristic(void) {
+    if (!BLE::Instance().gattClient().isServiceDiscoveryActive()) {
+        led_characteristic.read();
+    }
+}
+
+void characteristic_discovery(const DiscoveredCharacteristic *characteristicP) {
+    printf("  C UUID-%x valueAttr[%u] props[%x]\r\n", characteristicP->getUUID().getShortUUID(), characteristicP->getValueHandle(), (uint8_t)characteristicP->getProperties().broadcast());
+    if (characteristicP->getUUID().getShortUUID() == 0xa001) { /* !ALERT! Alter this filter to suit your device. */
+        led_characteristic        = *characteristicP;
+        trigger_led_characteristic = true;
+    }
+}
+
+void discovery_termination(Gap::Handle_t connectionHandle) {
+    printf("terminated SD for handle %u\r\n", connectionHandle);
+    if (trigger_led_characteristic) {
+        trigger_led_characteristic = false;
+        event_queue.call(update_led_characteristic);
+    }
+}
+
+void trigger_toggled_write(const GattReadCallbackParams *response) {
+    if (response->handle == led_characteristic.getValueHandle()) {
+        printf("trigger_toggled_write: handle %u, offset %u, len %u\r\n", response->handle, response->offset, response->len);
+        for (unsigned index = 0; index < response->len; index++) {
+            printf("%c[%02x]", response->data[index], response->data[index]);
+        }
+        printf("\r\n");
+
+        uint8_t toggledValue = response->data[0] ^ 0x1;
+        led_characteristic.write(1, &toggledValue);
+    }
+}
+
+void trigger_read(const GattWriteCallbackParams *response) {
+    if (response->handle == led_characteristic.getValueHandle()) {
+        led_characteristic.read();
+    }
+}
 
 class LEDDemo : ble::Gap::EventHandler {
 public:
@@ -31,15 +92,35 @@ public:
         _event_queue(event_queue),
         _alive_led(LED1, 1),
         _actuated_led(LED2, 0),
+        _is_connecting(false),
         _led_uuid(LEDService::LED_SERVICE_UUID),
         _led_service(NULL),
         _adv_data_builder(_adv_buffer) { }
 
-    ~LEDDemo() {
+    virtual ~LEDDemo() {
         delete _led_service;
     }
 
-    void start() {
+    virtual void start() {
+        ble_error_t error = _ble.gap().startScan();
+        if (error) {
+            print_error(error, "Error caused by Gap::startScan");
+            return;
+        }
+        start_advertising();
+    }
+
+    void stop() {
+        ble_error_t error = _ble.gap().stopScan();
+        if (error) {
+            print_error(error, "Error caused by Gap::stopScan");
+            return;
+        }
+        _is_connecting = false;
+        stop_advertising();
+    }
+
+    void init() {
         _ble.gap().setEventHandler(this);
 
         _ble.init(this, &LEDDemo::on_init_complete);
@@ -49,21 +130,8 @@ public:
         _event_queue.dispatch_forever();
     }
 
-private:
-    /** Callback triggered when the ble initialization process has finished */
-    void on_init_complete(BLE::InitializationCompleteCallbackContext *params) {
-        if (params->error != BLE_ERROR_NONE) {
-            printf("Ble initialization failed.");
-            return;
-        }
+    void stop_advertising() {
 
-        _led_service = new LEDService(_ble, false);
-
-        _ble.gattServer().onDataWritten(this, &LEDDemo::on_data_written);
-
-        print_mac_address();
-
-        start_advertising();
     }
 
     void start_advertising() {
@@ -101,12 +169,116 @@ private:
         }
 
         /* Start advertising */
-
+        printf("Starting advertisement\r\n");
         error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
 
         if (error) {
             printf("_ble.gap().startAdvertising() failed\r\n");
             return;
+        }
+    }
+
+    void onConnectionComplete(const ble::ConnectionCompleteEvent& event) {
+        _ble.gattClient().onServiceDiscoveryTermination(discovery_termination);
+        _ble.gattClient().launchServiceDiscovery(
+            event.getConnectionHandle(),
+            service_discovery,
+            characteristic_discovery,
+            BLE_UUID_UNKNOWN,
+            BLE_UUID_UNKNOWN
+        );
+        _is_connecting = false;
+    }
+
+    /** Callback triggered when the ble initialization process has finished */
+    void on_init_complete(BLE::InitializationCompleteCallbackContext *params) {
+        printf("Init complete\r\n");
+        if (params->error != BLE_ERROR_NONE) {
+            printf("Ble initialization failed.");
+            return;
+        }
+
+        print_mac_address();
+
+        _led_service = new LEDService(_ble, false);
+
+        _ble.gattServer().onDataWritten(this, &LEDDemo::on_data_written);
+        _ble.gattClient().onDataRead(trigger_toggled_write);
+
+        printf("Setting scan parameters\r\n");
+        ble::ScanParameters scan_params;
+        _ble.gap().setScanParameters(scan_params);
+        printf("Finished init code\r\n");
+
+        start();
+    }
+
+    void blink() {
+        _alive_led = !_alive_led;
+    }
+
+    /* Event handler */
+
+    void onDisconnectionComplete(const ble::DisconnectionCompleteEvent&) {
+        _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+        _is_connecting = false;
+    }
+
+    void onAdvertisingReport(const ble::AdvertisingReportEvent &event) {
+        /* don't bother with analysing scan result if we're already connecting */
+        if (_is_connecting) {
+            return;
+        }
+
+        ble::AdvertisingDataParser adv_data(event.getPayload());
+
+        /* parse the advertising payload, looking for a discoverable device */
+        while (adv_data.hasNext()) {
+            ble::AdvertisingDataParser::element_t field = adv_data.next();
+
+            /* connect to a discoverable device */
+            if (field.type == ble::adv_data_type_t::COMPLETE_LOCAL_NAME &&
+                field.value.size() == strlen(DEVICE_NAME) &&
+                (memcmp(field.value.data(), DEVICE_NAME, field.value.size()) == 0)) {
+
+                printf("Adv from: ");
+                printf("%02x:%02x:%02x:%02x:%02x:%02x\r\n",
+                       event.getPeerAddress().data()[5],
+                       event.getPeerAddress().data()[4],
+                       event.getPeerAddress().data()[3],
+                       event.getPeerAddress().data()[2],
+                       event.getPeerAddress().data()[1],
+                       event.getPeerAddress().data()[0]);
+                printf(" rssi: %d, scan response: %u, connectable: %u\r\n",
+                       event.getRssi(), event.getType().scan_response(), event.getType().connectable());
+
+                ble_error_t error = _ble.gap().stopScan();
+
+                if (error) {
+                    print_error(error, "Error caused by Gap::stopScan");
+                    return;
+                }
+
+                const ble::ConnectionParameters connection_params;
+
+                error = _ble.gap().connect(
+                    event.getPeerAddressType(),
+                    event.getPeerAddress(),
+                    connection_params
+                );
+
+                if (error) {
+                    _ble.gap().startScan();
+                    return;
+                }
+
+                /* we may have already scan events waiting
+                 * to be processed so we need to remember
+                 * that we are already connecting and ignore them */
+                _is_connecting = true;
+
+                return;
+            }
         }
     }
 
@@ -116,27 +288,18 @@ private:
      * @param[in] params Information about the characterisitc being updated.
      */
     void on_data_written(const GattWriteCallbackParams *params) {
+        // trigger_read(params);
         if ((params->handle == _led_service->getValueHandle()) && (params->len == 1)) {
             _actuated_led = *(params->data);
         }
     }
 
-    void blink() {
-        _alive_led = !_alive_led;
-    }
-
-private:
-    /* Event handler */
-
-    void onDisconnectionComplete(const ble::DisconnectionCompleteEvent&) {
-        _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
-    }
-
-private:
+protected:
     BLE &_ble;
     events::EventQueue &_event_queue;
     DigitalOut _alive_led;
     DigitalOut _actuated_led;
+    bool _is_connecting;
 
     UUID _led_uuid;
     LEDService *_led_service;
@@ -156,7 +319,7 @@ int main()
     ble.onEventsToProcess(schedule_ble_events);
 
     LEDDemo demo(ble, event_queue);
-    demo.start();
+    demo.init();
 
     return 0;
 }
